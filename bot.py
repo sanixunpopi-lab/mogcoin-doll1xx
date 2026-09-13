3cab9ef2e8b67d1e84c006ffb0ddc981e5e0568b
sanixunpopi-lab <sanixunpopi@gmail.com>
2026-09-13 23:39:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ import threading
 # =============================================
 # ===== НАСТРОЙКИ =====
 # =============================================
-BOT_TOKEN = "ТВОЙ_ТОКЕН_ОТ_BOTFATHER"
-WEBAPP_URL = "https://твой_юзернейм.github.io/mog-coin/"
+BOT_TOKEN = "8805424592:AAHVjlM6tuZ7pDT-1bpZ7yZDjfSxnDlRQl0"
+WEBAPP_URL = "https://sanixunpopi-lab.github.io/mogcoin-doll1xx/"
 ADMIN_ID = 8698280423
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
